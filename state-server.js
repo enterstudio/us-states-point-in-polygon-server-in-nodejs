d9e644c218ad674736bfb158e999c39debef1c9a
geoff-pitman <gpitm379@live.kutztown.edu>
2015-11-21 03:04:36 -0500

    Update state-server.js

--- state-server.js
+++ state-server.js
@@ -37,7 +37,8 @@ app.post('/', function(req, res){
 var server = app.listen(3000, function (){
   var host = server.address().address;
   var port = server.address().port;
-  
+
+// blocking call at server start to load border points in  
   lineReader.eachLine('states.json', function(line, last) {
 	states.push(JSON.parse(line));
   });
